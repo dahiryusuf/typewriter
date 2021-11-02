78367809d08850ab3425fd6e51b7a2489d57f39a
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-02 23:46:27 +0000

    typewriter

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+let timer = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, timer);
+  timer += 900;
+}
+setTimeout(() => {
+  process.stdout.write("\n");
+}, timer);
+
+
+
